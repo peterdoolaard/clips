aff169701aedc41d149ce4141f4c7a3c8bac2f55
peterdoolaard <peter.doolaard@xs4all.nl>
2023-03-19 17:28:03 +0100

    Adding user module, shared module, modal component

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/app/**/*.{html,ts}'
+  ],
+  theme: {
+    fontFamily: {
+      'sans': ['Roboto, ui-sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
