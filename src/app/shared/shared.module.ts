7908553fd9b91b1aced7d65bcb57b505793fa414
peterdoolaard <peter.doolaard@xs4all.nl>
2023-03-20 19:29:18 +0100

    Setting up tabbed interface

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { TabComponent } from './tab/tab.component';
 
 
 
 @NgModule({
   declarations: [
-    ModalComponent
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent
   ],
   exports: [
-    ModalComponent
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent
   ],
   imports: [
     CommonModule
