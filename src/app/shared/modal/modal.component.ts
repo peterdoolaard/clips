c25d5d0e4561938c7d3015e93fcdb71093a8ec40
peterdoolaard <peter.doolaard@xs4all.nl>
2023-03-20 19:12:12 +0100

    Improving the modals

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, ElementRef, OnInit } from '@angular/core';
 import { ModalService } from "../../services/modal.service";
+
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
+
+  @Input() modalID = ''
 
   constructor(
-    public modal: ModalService
-  ) {
-    console.log(this.modal.visible)
+    public modal: ModalService,
+    public element: ElementRef
+  ) {}
+
+  ngOnInit() {
+    document.body.appendChild(this.element.nativeElement)
+  }
+
+  closeModal() {
+    this.modal.toggleModal(this.modalID)
   }
 
 }
--- src/app/user/auth-modal/auth-modal.component.ts
+++ src/app/user/auth-modal/auth-modal.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ModalService } from "../../services/modal.service";
 
 @Component({
   selector: 'app-auth-modal',
   templateUrl: './auth-modal.component.html',
   styleUrls: ['./auth-modal.component.scss']
 })
-export class AuthModalComponent {
+export class AuthModalComponent implements OnInit, OnDestroy {
 
+  constructor(
+    public modal: ModalService
+  ) {
+  }
+
+  ngOnInit() {
+    this.modal.register('auth')
+  }
+
+  ngOnDestroy() {
+    this.modal.unregister('auth')
+  }
 }
