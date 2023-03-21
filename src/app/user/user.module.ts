e3e7f3c1c4d67617e6dabead8a6e96007abfeffe
peterdoolaard <peter.doolaard@xs4all.nl>
2023-03-21 17:09:28 +0100

    Setting up forms

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from "@angular/forms";
+
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
 import  { SharedModule } from "../shared/shared.module";
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
-    AuthModalComponent
+    AuthModalComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   exports: [
     AuthModalComponent
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ]
 })
 export class UserModule { }
