e2ee4a6352d90855bec93eb012c042671d18b97b
peterdoolaard <peter.doolaard@xs4all.nl>
2023-03-21 16:35:34 +0100

    Finishing tabbed interface

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, ContentChildren, QueryList } from '@angular/core';
+import { TabComponent } from "../tab/tab.component";
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
   styleUrls: ['./tabs-container.component.scss']
 })
-export class TabsContainerComponent {
+export class TabsContainerComponent implements AfterContentInit {
+
+  @ContentChildren(TabComponent) tabs?: QueryList<TabComponent>
+
+  ngAfterContentInit() {
+    const activeTabs = this.tabs?.filter(tab => tab.active === true)
+
+    if (!activeTabs || activeTabs.length === 0) {
+      this.selectTab(this.tabs!.first)
+    }
+  }
+
+  selectTab(tab: TabComponent) {
+    this.tabs?.forEach(tab => tab.active = false)
+    tab.active = true
+    return false
+  }
 
 }
